e183a2ba6eeef0d6a2d4c29faa2af000451f1e1a
tanzaniagetawayinfo-source <tanzaniagetaway.info@gmail.com>
2026-06-22 11:52:52 +0300

    Add minimal visit tracker

--- site-analytics.js
+++ site-analytics.js
@@ -1,1 +1,9 @@
-(function(){console.log('ftg analytics placeholder')})();
+(function(){
+  if(window.__FTG_SITE_ANALYTICS__) return;
+  window.__FTG_SITE_ANALYTICS__ = true;
+  if(/^localhost$|^127\./.test(location.hostname)) return;
+  try {
+    var img = new Image();
+    img.src = 'https://jpflcbktcnhmlvaibzcw.supabase.co/functions/v1/site-visit?page_path=' + encodeURIComponent(location.pathname || '/') + '&page_title=' + encodeURIComponent(document.title || '') + '&ts=' + Date.now();
+  } catch (e) {}
+})();
